b7edc40661058ce0783be0f42c5dc00c8e3ecd20
Novak Istvan <novak.istvan.ifj@gmail.com>
2026-02-22 13:55:32 +0100

    fix readme, start_dev

--- start_dev.ps1
+++ start_dev.ps1
@@ -1,13 +1,13 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-Write-Host "Waiting for ArgoCD deployments..."
+Write-Host "argocd deployment waiting"
 
 kubectl wait --for=condition=available deployment/argocd-server -n argocd --timeout=180s
 kubectl wait --for=condition=available deployment/argocd-repo-server -n argocd --timeout=180s
 kubectl wait --for=condition=available deployment/argocd-application-controller -n argocd --timeout=180s
 kubectl wait --for=condition=available deployment/argocd-redis -n argocd --timeout=180s
 
-Write-Host "ArgoCD ready. Deploying root app..."
+Write-Host "argocd ready, wait app"
 
 kubectl apply -f root-app.yaml -n argocd
